6001cb050bb2c1daf97def5810fa2313338b9094
PRATAP-KUMAR <pratap@fastmail.fm>
2022-07-19 22:38:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const qs = function (selector = '*', element = document) {
-    return element.querySelector(selector);
-}
+const qs = function querySelector(selector = '*', element = document) {
+  return element.querySelector(selector);
+};
 
 const hamburger = qs('#hamburger');
 const closeIcon = qs('#close-icon');
 const container = qs('nav.nav ul');
 
-const toggleBurger = function () {
-    hamburger.classList.toggle('no-display');
-    closeIcon.classList.toggle('no-display');
-    container.classList.toggle('enlarge');
-}
+const toggleBurger = function toggleBurger() {
+  hamburger.classList.toggle('no-display');
+  closeIcon.classList.toggle('no-display');
+  container.classList.toggle('enlarge');
+};
 
-hamburger.addEventListener('click', function () {
-    toggleBurger();
+hamburger.addEventListener('click', () => {
+  toggleBurger();
 });
 
-closeIcon.addEventListener('click', function () {
-    toggleBurger();
+closeIcon.addEventListener('click', () => {
+  toggleBurger();
 });
 
-container.addEventListener('click', function (e) {
-    const { target } = e
+container.addEventListener('click', (e) => {
+  const { target } = e;
 
-    if (target.tagName.toLowerCase() !== 'a') return
+  if (target.tagName.toLowerCase() !== 'a') return;
 
-    toggleBurger();
-    qs(target.getAttribute('href')).scrollIntoView();
+  toggleBurger();
+  qs(target.getAttribute('href')).scrollIntoView();
 });
 
 window.onresize = () => {
-    if (window.innerWidth >= 768 && container.classList.contains('enlarge')) {
-        toggleBurger();
-    }
-}
+  if (window.innerWidth >= 768 && container.classList.contains('enlarge')) {
+    toggleBurger();
+  }
+};
