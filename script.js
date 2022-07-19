c529c4cdef6c79a8ffccc057398047854426f60f
serialferris <57386627+serialferris@users.noreply.github.com>
2022-07-19 19:27:04 +0530

    updated index.html style.css and script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const hamburger = document.querySelector(".mobile-visible");
-const navMenu = document.querySelector(".desktop-visible");
+const qs = function (selector = '*', element = document) {
+    return element.querySelector(selector);
+}
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+const hamburger = qs('#hamburger');
+const closeIcon = qs('#close-icon');
+const container = qs('nav.nav ul');
 
-document.querySelectorAll(".desktop-visible").forEach(n => n.addEventListener("click", () => {
-	hamburger.classList.remove("active");
-	navMenu.classList.remove("active");
-}))
+const toggleBurger = function () {
+    hamburger.classList.toggle('no-display');
+    closeIcon.classList.toggle('no-display');
+    container.classList.toggle('enlarge');
+}
+
+hamburger.addEventListener('click', function () {
+    toggleBurger();
+});
+
+closeIcon.addEventListener('click', function () {
+    toggleBurger();
+});
+
+container.addEventListener('click', function (e) {
+    const { target } = e
+
+    if (target.tagName.toLowerCase() !== 'a') return
+
+    toggleBurger();
+    qs(target.getAttribute('href')).scrollIntoView();
+});
