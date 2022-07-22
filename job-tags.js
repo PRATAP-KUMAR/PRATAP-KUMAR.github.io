eb07ea18e714247787d35c67d5a6754c89f7ffa1
PRATAP-KUMAR <pratap@fastmail.fm>
2022-07-22 11:20:40 +0530

    Update html, css and js files

---
+++ job-tags.js
@@ -0,0 +1,12 @@
+const jobTags = (tags) => {
+    const element = document.createElement('ul');
+    tags.forEach((tagContent) => {
+        const tag = document.createElement('li');
+        tag.textContent = tagContent;
+        element.appendChild(tag);
+    });
+
+    return element;
+};
+
+export default jobTags;
