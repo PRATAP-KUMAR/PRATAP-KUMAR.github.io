77e11f9bbed58f6d52b4bd9efca45edf71b4c2a9
PRATAP-KUMAR <pratap@fastmail.fm>
2022-07-23 20:36:24 +0530

    Fix linter errors

--- validation.js
+++ validation.js
@@ -1,24 +1,21 @@
-const form = document.querySeelector("#contact-form");
-const EMAIL_CASE_SENSITIVITY_CHECK = "Please enter the letters in your email as lower case";
+const form = document.querySeelector('#contact-form');
+const EMAIL_CASE_SENSITIVITY_CHECK = 'Please enter the letters in your email as lower case';
 
-form.addEventListener("submit", (event) => {
-	event.preventDefault();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-	let email = form.elements["email"].value;
-	let button = form.elements["button"];
+  const email = form.elements.email.value;
+  const { button } = form.elements;
 
-	const errorMessage = document.createElement('p');
-	errorMessage.textAlign = 'left';
-	errorMessage.color = '#ff0000';
-	errorMessage.fontSize = '32px';
-	button.insertAdjacentElement('beforebegin', errSpan);
+  const errorMessage = document.createElement('p');
+  errorMessage.textAlign = 'left';
+  errorMessage.color = '#ff0000';
+  errorMessage.fontSize = '32px';
+  button.insertAdjacentElement('beforebegin', errorMessage);
 
-
-	if (email.value !== email.toLocaleLowerCase()) {
-		errorMessage.innerText = EMAIL_CASE_SENSITIVITY_CHECK;
-	} else {
-		form.submit();
-	}
+  if (email.value !== email.toLocaleLowerCase()) {
+    errorMessage.innerText = EMAIL_CASE_SENSITIVITY_CHECK;
+  } else {
+    form.submit();
+  }
 });
-
-
