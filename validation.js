817afb46d6a4b6ee7e3c9c4766a49aea26d12004
PRATAP <pratap@fastmail.fm>
2022-07-24 00:01:58 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,17 +1,17 @@
-const form = document.querySeelector('#contact-form');
+const form = document.querySelector('#contact-form');
 const EMAIL_CASE_SENSITIVITY_CHECK = 'Please enter the letters in your email as lower case';
+
+const { email, button } = form.elements;
+
+const errorMessage = document.createElement('p');
+errorMessage.textAlign = 'left';
+errorMessage.color = '#ff0000';
+errorMessage.fontSize = '32px';
+button.insertAdjacentElement('beforebegin', errorMessage);
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const email = form.elements.email.value;
-  const { button } = form.elements;
-
-  const errorMessage = document.createElement('p');
-  errorMessage.textAlign = 'left';
-  errorMessage.color = '#ff0000';
-  errorMessage.fontSize = '32px';
-  button.insertAdjacentElement('beforebegin', errorMessage);
 
   if (email.value !== email.toLocaleLowerCase()) {
     errorMessage.innerText = EMAIL_CASE_SENSITIVITY_CHECK;
