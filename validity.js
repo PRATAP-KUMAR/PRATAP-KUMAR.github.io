40420a1d9cbfdb045f279fbd9ff1ed95076b7c47
PRATAP-KUMAR <pratap@fastmail.fm>
2022-07-24 02:12:58 +0530

    Update files

---
+++ validity.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form');
+
+const { email, button } = form.elements;
+
+const errSpan = document.createElement('p');
+errSpan.style.textAlign = 'left';
+errSpan.style.color = 'red';
+errSpan.style.fontSize = '1rem';
+button.insertAdjacentElement('beforebegin', errSpan);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (email.value !== email.value.toLowerCase()) {
+    invalidMessage.textContent = 'please type your whole email address in lowercase';
+  } else {
+    invalidMessage.textContent = '';
+    form.submit();
+  }
+});
