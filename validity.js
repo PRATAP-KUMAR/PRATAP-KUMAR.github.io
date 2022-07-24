e1d69c243120d6cad3556af260cfc910008130d2
PRATAP-KUMAR <pratap@fastmail.fm>
2022-07-25 03:50:49 +0530

    linters corrected

--- validity.js
+++ validity.js
@@ -1,14 +1,11 @@
-const form = document.forms["contactForm"];
-const button = form["button"];
-const errorMessageElement = document.createElement('a');
-errorMessageElement.style.textAlign = 'center';
+const form = document.forms.contactForm;
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  if (form['email'].value === form['email'].value.toLowerCase()) {
+  if (form.email.value === form.email.value.toLowerCase()) {
     form.submit();
   } else {
-    alert("Please write email letters in small case")
+    alert('Please write email letters in small case');
   }
-}
-)
+});
